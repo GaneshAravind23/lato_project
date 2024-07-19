9767a6a8fd54763f024ac6dac8802c4b7157cb51
ganesharavimth341 <ganesharavimth341@gmail.com>
2024-07-19 10:13:39 +0530

    structure_skillset

--- career_project/src/app/app.module.ts
+++ career_project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillsetComponent } from './skillset/skillset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsetComponent
   ],
   imports: [
     BrowserModule,
